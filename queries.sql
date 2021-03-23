08121ef7316eb464abc561c3c46c0bed0c346332
Christopher <c.millerscott@gmail.com>
2021-03-23 08:50:02 -0500

    steps 1 & 2 complete

--- queries.sql
+++ queries.sql
@@ -1,6 +1,14 @@
 ## Part 1: Test it with SQL
 
+SELECT column_name, data_type
+FROM information_schema.columns
+WHERE table_schema = 'techjobs' AND table_name = 'job';
+
 ## Part 2: Test it with SQL
+
+SELECT name
+FROM employer
+WHERE location = "St. Louis City";
 
 ## Part 3: Test it with SQL
 
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,26 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @Size(max = 200, message = "Description must be 200 characters or fewer.")
+    private String description;
+
+    public Skill(String description) {
+        this.description = description;
+    }
+
+    public Skill(){}
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
