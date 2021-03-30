80c55862d54cc87b8df74f57bc599e05fcbbda45
Christopher <c.millerscott@gmail.com>
2021-03-30 13:56:14 -0500

    1-3 complete

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,11 +1,19 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
+
+    @OneToMany
+    @JoinColumn(name = "employer_id")
+    private List<Job> jobs = new ArrayList <>();
 
     @NotBlank(message = "Location is required.")
     @Size(min = 3, max = 50, message = "Location must be between 3 and 50 characters.")
@@ -16,6 +24,10 @@ public class Employer extends AbstractEntity {
     }
 
     public Employer() {
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
     }
 
     public String getLocation() {
